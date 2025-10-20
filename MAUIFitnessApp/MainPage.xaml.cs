15876720fb44dd6c417c7a112019893b1ed015bb
GRainbowP <uT_3775@zsp3zamosc.pl>
2025-10-20 12:46:23 +0200

    Initial commit

---
+++ MAUIFitnessApp/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MAUIFitnessApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
